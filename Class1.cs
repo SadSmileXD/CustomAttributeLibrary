8b46800391bff4d16f76153e0d30f6a44d212b56
SadSmileXD <phycoeco@naver.com>
2025-05-15 17:19:16 +0900

    인스펙트 화면 enum 커스텀 표시

---
+++ Class1.cs
@@ -0,0 +1,58 @@
+﻿
+using UnityEngine;
+using UnityEditor;
+using System;
+using System.Linq;
+using System.Reflection;
+
+// 모든 enum 타입에 적용
+[CustomPropertyDrawer(typeof(Enum), true)]
+public class GenericEnumDisplayNameDrawer : PropertyDrawer
+{
+    public override bool CanCacheInspectorGUI(SerializedProperty property) => false;
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        if (property.propertyType != SerializedPropertyType.Enum)
+        {
+            EditorGUI.PropertyField(position, property, label, true);
+            return;
+        }
+
+        var enumType = fieldInfo.FieldType;
+        var enumValues = Enum.GetValues(enumType);
+        string[] displayNames = new string[enumValues.Length];
+
+        for (int i = 0; i < enumValues.Length; i++)
+        {
+            var field = enumType.GetField(enumValues.GetValue(i).ToString());
+            var attr = field?.GetCustomAttribute<EnumDisplayNameAttribute>();
+            displayNames[i] = attr != null ? attr.DisplayName : field.Name;
+        }
+
+        int selectedIndex = property.enumValueIndex;
+        selectedIndex = EditorGUI.Popup(position, label.text, selectedIndex, displayNames);
+        property.enumValueIndex = selectedIndex;
+    }
+}
+[AttributeUsage(AttributeTargets.Field)]
+public class EnumDisplayNameAttribute : Attribute
+{
+    public string DisplayName { get; }
+
+    public EnumDisplayNameAttribute(string name)
+    {
+        DisplayName = name;
+    }
+}
+/*
+ 종속성->참조추가
+using UnityEngine;
+using UnityEditor;
+UnityEngine.IMGUIModule.dll
+UnityEngine.CoreModule.dll
+UnityEditor.CoreModule.dll
+참조해야함
+
+ */
+
