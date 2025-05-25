1d121affe8fb6727144660c06574c69af1165ab0
SadSmileXD <phycoeco@naver.com>
2025-05-26 00:27:22 +0900

    인스펙트 창 한글 변화 추가

--- Class1.cs
+++ Class1.cs
@@ -4,47 +4,58 @@ using UnityEditor;
 using System;
 using System.Linq;
 using System.Reflection;
+ 
 
-// 모든 enum 타입에 적용
-[CustomPropertyDrawer(typeof(Enum), true)]
-public class GenericEnumDisplayNameDrawer : PropertyDrawer
+namespace customlibrary
 {
-    public override bool CanCacheInspectorGUI(SerializedProperty property) => false;
-
-    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+  
+    // 모든 enum 타입에 적용
+    [CustomPropertyDrawer(typeof(Enum), true)]
+    public class GenericEnumDisplayNameDrawer : PropertyDrawer
     {
-        if (property.propertyType != SerializedPropertyType.Enum)
+        public override bool CanCacheInspectorGUI(SerializedProperty property) => false;
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-            EditorGUI.PropertyField(position, property, label, true);
-            return;
+            if (property.propertyType != SerializedPropertyType.Enum)
+            {
+                EditorGUI.PropertyField(position, property, label, true);
+                return;
+            }
+
+            var enumType = fieldInfo.FieldType;
+            var enumValues = Enum.GetValues(enumType);
+            string[] displayNames = new string[enumValues.Length];
+
+            for (int i = 0; i < enumValues.Length; i++)
+            {
+                var field = enumType.GetField(enumValues.GetValue(i).ToString());
+                var attr = field?.GetCustomAttribute<EnumDisplayNameAttribute>();
+                displayNames[i] = attr != null ? attr.DisplayName : field.Name;
+            }
+
+            int selectedIndex = property.enumValueIndex;
+            selectedIndex = EditorGUI.Popup(position, label.text, selectedIndex, displayNames);
+            property.enumValueIndex = selectedIndex;
         }
-
-        var enumType = fieldInfo.FieldType;
-        var enumValues = Enum.GetValues(enumType);
-        string[] displayNames = new string[enumValues.Length];
-
-        for (int i = 0; i < enumValues.Length; i++)
-        {
-            var field = enumType.GetField(enumValues.GetValue(i).ToString());
-            var attr = field?.GetCustomAttribute<EnumDisplayNameAttribute>();
-            displayNames[i] = attr != null ? attr.DisplayName : field.Name;
-        }
-
-        int selectedIndex = property.enumValueIndex;
-        selectedIndex = EditorGUI.Popup(position, label.text, selectedIndex, displayNames);
-        property.enumValueIndex = selectedIndex;
     }
-}
-[AttributeUsage(AttributeTargets.Field)]
-public class EnumDisplayNameAttribute : Attribute
-{
-    public string DisplayName { get; }
-
-    public EnumDisplayNameAttribute(string name)
+    [AttributeUsage(AttributeTargets.Field)]
+    public class EnumDisplayNameAttribute : Attribute
     {
-        DisplayName = name;
+        public string DisplayName { get; }
+
+        public EnumDisplayNameAttribute(string name)
+        {
+            DisplayName = name;
+        }
     }
+
+
+ 
+  
+
 }
+
 /*
  종속성->참조추가
 using UnityEngine;
---
+++ PropertyDrawer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+ namespace customlibrary
+{
+    public class InspectorNameAttribute : PropertyAttribute
+    {
+        public string displayName;
+        public InspectorNameAttribute(string name)
+        {
+            displayName = name;
+        }
+    }
+    [CustomPropertyDrawer(typeof(InspectorNameAttribute))]
+    public class InspectorNameDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            InspectorNameAttribute attr = (InspectorNameAttribute)attribute;
+            label.text = attr.displayName;
+            EditorGUI.PropertyField(position, property, label);
+        }
+    }
+}
